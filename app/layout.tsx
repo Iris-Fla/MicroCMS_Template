0d225c891acb8e4dc0714da32b9c5c8b25433bce
Meru <103801589+Iris-Fla@users.noreply.github.com>
2023-07-18 16:14:11 +0900

    Faviconの追加★

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,15 @@ export default async function RootLayout({ children }: Props) {
   });
   return (
     <html lang="ja">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         <div className={KiwiMaru.className}>
           <Header />
